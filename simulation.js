a2da6e79ced54a0b49ca846197bd0dace2f17936
Sam Washington <sam@spwashi.com>
2023-11-04 10:43:23 -0500

    simulation: tuning
    updated simulation parameters to be more stable under current node and
    link relationships

--- simulation.js
+++ simulation.js
@@ -1,19 +1,27 @@
-const width = 400, height = 300;
+const width = 400;
+const height = 300;
+const X_START_POS = -4900;
+const Y_START_POS = -1800;
 
 const nodes = makeStarterNodes(13);
 const links = makeStarterLinks(nodes);
+
 const simulation =
 	d3.forceSimulation(nodes)
 		.alphaTarget(.3)
 		.force('charge', d3.forceManyBody().strength(-500))
-		.force('center', d3.forceCenter(width / 2, height / 2))
+		.force('center', d3.forceCenter(width / 2, height / 2).strength(.02))
 		.force('link', d3.forceLink().links(links))
 		.on('tick', getTicker(initDragBehavior()));
 
 function makeStarterNodes(count) {
 	const nodes = [];
 	for (let i = 0; i <= count; i++) {
-		nodes.push({name: i, x: 0, y: 0});
+		nodes.push({
+			name: i,
+			x: X_START_POS,
+			y: Y_START_POS,
+		});
 	}
 	return nodes;
 }
