93ec7d222d71fe784576591b572c825c23138fe6
Sam Washington <sam@spwashi.com>
2023-11-04 10:18:34 -0500

    simulation: now generating links and nodes

--- simulation.js
+++ simulation.js
@@ -1,30 +1,27 @@
-const nodes = [
-	{name: 'A', x: 0, y: 0},
-	{name: 'B', x: 0, y: 0},
-	{name: 'C', x: 0, y: 0},
-	{name: 'D', x: 0, y: 0},
-	{name: 'E', x: 0, y: 0},
-	{name: 'F', x: 0, y: 0},
-	{name: 'G', x: 0, y: 0},
-	{name: 'H', x: 0, y: 0},
-]
-
-const links = [
-	{source: 0, target: 1},
-	{source: 0, target: 2},
-	{source: 0, target: 3},
-	{source: 1, target: 6},
-	{source: 3, target: 4},
-	{source: 3, target: 7},
-	{source: 4, target: 5},
-	{source: 4, target: 7}
-]
-
 const width = 400, height = 300;
+
+const nodes = makeStarterNodes(13);
+const links = makeStarterLinks(nodes);
 const simulation =
 	d3.forceSimulation(nodes)
 		.alphaTarget(.3)
-		.force('charge', d3.forceManyBody().strength(-100))
+		.force('charge', d3.forceManyBody().strength(-500))
 		.force('center', d3.forceCenter(width / 2, height / 2))
 		.force('link', d3.forceLink().links(links))
 		.on('tick', getTicker(initDragBehavior()));
+
+function makeStarterNodes(count) {
+	const nodes = [];
+	for (let i = 0; i <= count; i++) {
+		nodes.push({name: i, x: 0, y: 0});
+	}
+	return nodes;
+}
+function makeStarterLinks(nodes) {
+	const links = [];
+	for (let node of nodes) {
+		links.push({source: 0, target: node.name});	
+	}
+
+	return links;
+}
