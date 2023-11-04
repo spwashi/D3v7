070a26bf5aff9f6eac0ad0202d9eeb8a2869e5d7
Sam Washington <sam@spwashi.com>
2023-11-04 11:44:47 -0500

    refactor: variable extraction

--- js/main.js
+++ js/main.js
@@ -1,68 +1,80 @@
+const colors = ['#ffffff', '#556677'];
+const getNodeColor = d => colors[(d.index || 0) % colors.length];
+
+const nodesManager = {
+	update: 
+		function updateNodes(drag) {
+			const dataSelection = d3.select('.nodes')
+				.selectAll('g')
+				.data(nodes, d => d.name)
+			;
+			dataSelection
+				.join(
+					enter => {
+						const g = 
+							enter.append('g')
+							.classed('node', true)
+							.attr('id', d => d.name);
+
+						g
+							.append('circle')
+							.attr('fill', getNodeColor)
+							.attr('r', d => d.r || 1)
+							.attr('cx', d => d.x || 0)
+							.attr('cy', d => d.y || 0)
+							.call(drag);
+						g
+							.append('text')
+							.text(d => d.name)
+							.attr('font-size', d => d.r || 10)
+							.attr('x', d => d.x || 0)
+							.attr('y', d => d.y || 0)
+						return g;
+					},
+					update => {
+						update
+							.select('circle')
+							.attr('cx', d => d.x || 0)
+							.attr('cy', d => d.y || 0)
+						update
+							.select('text')
+							.attr('x', d => d.x || 0)
+							.attr('y', d => d.y || 0)
+						return update;
+					},
+					remove => remove.remove()
+				);
+		}
+};
+const linksManager = {
+	update: 
+		function updateLinks() {
+			const u = d3.select('.links')
+				.selectAll('line')
+				.data(links)
+				.join('line')
+				.attr('x1', function(d) {
+					return d.source.x
+				})
+				.attr('y1', function(d) {
+					return d.source.y
+				})
+				.attr('x2', function(d) {
+					return d.target.x
+				})
+				.attr('y2', function(d) {
+					return d.target.y
+				});
+		}
+}
+
 function getTicker(drag) {
+	const updateLinks = linksManager.update;
+	const updateNodes = nodesManager.update;
 	function tick() {
 		updateLinks();
 		updateNodes(drag);
 	}
 
 	return tick;
-}
-
-function updateLinks() {
-	const u = d3.select('.links')
-		.selectAll('line')
-		.data(links)
-		.join('line')
-		.attr('x1', function(d) {
-			return d.source.x
-		})
-		.attr('y1', function(d) {
-			return d.source.y
-		})
-		.attr('x2', function(d) {
-			return d.target.x
-		})
-		.attr('y2', function(d) {
-			return d.target.y
-		});
-}
-
-function updateNodes(drag) {
-	const dataSelection = d3.select('.nodes')
-		.selectAll('g')
-		.data(nodes, d => d.name)
-	;
-	dataSelection
-		.join(
-			enter => {
-				const g = 
-					enter.append('g')
-					.classed('node', true)
-					.attr('id', d => d.name);
-
-				g
-					.append('circle')
-					.attr('r', 10)
-					.attr('cx', d => d.x)
-					.attr('cy', d => d.y)
-					.call(drag);
-				g
-					.append('text')
-					.text(d => d.name)
-					.attr('x', d => d.x)
-					.attr('y', d => d.y)
-				return g;
-			},
-			update => {
-				update
-					.select('circle')
-					.attr('cx', d => d.x)
-					.attr('cy', d => d.y)
-				update
-					.select('text')
-					.attr('x', d => d.x)
-					.attr('y', d => d.y)
-				return update;
-			},
-			remove => remove.remove()
-		);
 }
--- js/simulation.js
+++ js/simulation.js
@@ -2,15 +2,19 @@ const width = 400;
 const height = 300;
 const X_START_POS = -4900;
 const Y_START_POS = -1800;
+const NODE_COUNT = 13;
+const CHARGE_STRENGTH = -500;
+const CENTER_STRENGTH = .1;
+const CENTER_POS = [width/2, height/2];
 
-const nodes = makeStarterNodes(13);
+const nodes = makeStarterNodes(NODE_COUNT);
 const links = makeStarterLinks(nodes);
 
 const simulation =
 	d3.forceSimulation(nodes)
 		.alphaTarget(.3)
-		.force('charge', d3.forceManyBody().strength(-500))
-		.force('center', d3.forceCenter(width / 2, height / 2).strength(.02))
+		.force('charge', d3.forceManyBody().strength(CHARGE_STRENGTH))
+		.force('center', d3.forceCenter(...CENTER_POS).strength(CENTER_STRENGTH))
 		.force('link', d3.forceLink().links(links))
 		.on('tick', getTicker(initDragBehavior()));
 
@@ -21,6 +25,7 @@ function makeStarterNodes(count) {
 			name: i,
 			x: X_START_POS,
 			y: Y_START_POS,
+			r: i % 13
 		});
 	}
 	return nodes;
