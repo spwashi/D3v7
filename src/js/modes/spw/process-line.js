946fab55b4775cfb4622b486dbe2e0e70b8be08b
Sam Washington <sam@spwashi.com>
2023-12-04 11:27:23 -0600

    renamed box command

--- src/js/modes/spw/process-line.js
+++ src/js/modes/spw/process-line.js
@@ -30,8 +30,8 @@ const patternsAndHandlers = {
       sideEffects.physicsChange = true;
     }
   },
-  boundingBox:     {
-    regex:   /^boundingBox=(-?\d+)/,
+  box:             {
+    regex:   /^box=(-?\d+)/,
     handler: (sideEffects, value) => {
       window.spwashi.parameters.forces.boundingBox = !!parseInt(value);
       sideEffects.physicsChange                    = true;
@@ -142,18 +142,18 @@ const patternsAndHandlers = {
       const choice  = value;
       const nodes   = window.spwashi.nodes;
       const options = {
-        "name":        (a, b) => a.name.localeCompare(b.name),
-        "name+":       (a, b) => a.name.localeCompare(b.name),
-        "name-":       (a, b) => b.name.localeCompare(a.name),
-        "r":           (a, b) => a.r - b.r,
-        "r+":          (a, b) => a.r - b.r,
-        "r-":          (a, b) => b.r - a.r,
+        "name":     (a, b) => a.name.localeCompare(b.name),
+        "name+":    (a, b) => a.name.localeCompare(b.name),
+        "name-":    (a, b) => b.name.localeCompare(a.name),
+        "r":        (a, b) => a.r - b.r,
+        "r+":       (a, b) => a.r - b.r,
+        "r-":       (a, b) => b.r - a.r,
         "cluster":  (a, b) => a.colorindex - b.colorindex,
         "cluster-": (a, b) => a.colorindex - b.colorindex,
         "cluster+": (a, b) => b.colorindex - a.colorindex,
-        "z":           (a, b) => a.z - b.z,
-        "z+":          (a, b) => a.z - b.z,
-        "z-":          (a, b) => b.z - a.z,
+        "z":        (a, b) => a.z - b.z,
+        "z+":       (a, b) => a.z - b.z,
+        "z-":       (a, b) => b.z - a.z,
       };
       const sort    = options[choice];
       if (!sort) return;
