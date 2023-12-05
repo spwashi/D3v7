9df7f629ab71aed97efd0990659f81abf71d7ee7
Sam Washington <sam@spwashi.com>
2023-12-05 11:04:20 -0600

    public dir, manifest

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite';
-import path from 'path';
+import {defineConfig} from 'vite';
+import path           from 'path';
 
-export default defineConfig({
-  root: path.resolve(__dirname, 'src'),
-  base: '/',
-  build: {
-    outDir: path.resolve(__dirname, '_site'),
-    emptyOutDir: true,
-    sourcemap: true,
-  },
-  server: {
-    host: 'localhost',
-    port: 3000,
-    open: true,
-  },
-  plugins: []
-});
+export default defineConfig(
+  {
+    root:      path.resolve(__dirname, 'src'),
+    base:      '/',
+    publicDir: path.resolve(__dirname, 'public'),
+    build:     {
+      outDir:      path.resolve(__dirname, '_site'),
+      emptyOutDir: true,
+      sourcemap:   true,
+    },
+    server:    {
+      host: 'localhost',
+      port: 3000,
+      open: true,
+    },
+    plugins:   []
+  }
+);
 
