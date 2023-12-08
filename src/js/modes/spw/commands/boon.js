1f341b9372cc7d304182d6b3def8a7a806f7920d
Sam Washington <sam@spwashi.com>
2023-12-07 20:49:57 -0600

    _spwashi@

--- src/js/modes/spw/commands/boon.js
+++ src/js/modes/spw/commands/boon.js
@@ -8,14 +8,15 @@ export function boonNode(i, nodeCount) {
   const sliceWidth = (window.spwashi.parameters.width * .9) / nodeCount;
 
   return {
-    name:       '#',
-    kind:       '__boon',
-    r:          50,
-    fx:         (sliceWidth * (i)) + (sliceWidth * (nodeCount - 1) / nodeCount),
-    fy:         window.spwashi.parameters.height / 2,
-    color:      'turquoise',
-    stroke:     '#222222',
-    colorindex: getDocumentDataIndex()
+    name:        '#',
+    kind:        '__boon',
+    r:           50,
+    fx:          (sliceWidth * (i)) + (sliceWidth * (nodeCount - 1) / nodeCount),
+    fy:          window.spwashi.parameters.height / 2,
+    color:       'turquoise',
+    stroke:      '#ffffff',
+    strokeWidth: 1,
+    colorindex:  getDocumentDataIndex()
   };
 }
 
